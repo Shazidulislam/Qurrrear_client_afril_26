a8a940edb607409130be36961421b72542461629
Shazidulislam <shazidulislam910@gmail.com>
2026-05-09 22:01:45 +0600

    create login and signup use react hook fprm

--- src/Pageses/Auth/Login/Login.jsx
+++ src/Pageses/Auth/Login/Login.jsx
@@ -1,42 +1,70 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LoginWithGoogle from "../LoginwithGoogle/loginWithGoogle";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <div className="space-y-3" >
+      <div className="space-y-3">
         <h1 className="text-3xl sm:text-4xl font-bold">Welcome Back</h1>
         <p>Login with profast</p>
       </div>
       {/* from */}
-      <form >
-        <fieldset className="fieldset space-y-1 mt-6 w-full" >  
-            <div className="space-y-3">
-              <label className="text-xs  font-medium">
-               Your Email
-              </label >
-              <input
-                className="input w-full border-2 mt-2 outline-none border-[#CBD5E1]  shadow-none"
-                placeholder="youremail@gmail.com"
-              />
-            </div>
-            <div className="space-y-3">
-              <label className="text-xs  font-medium">
-              Password
-              </label>
-              <input
-                className="input w-full border-2 mt-2 outline-none border-[#CBD5E1]  shadow-none"
-                placeholder="Password"
-              />
-            </div>
-            <div className="space-y-3" >
-              <p className="text-s  underline" >Forget Password?</p>
-              <button className="btn w-full bg-[#CAEB66]" >Login</button>
-              <p>Don’t have any account? <Link className="underline text-blue-600" to={"/signup"} >Register</Link>  </p>
-            </div>
-            <p className="text-center text-xs" >Or</p>
-            <LoginWithGoogle/>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <fieldset className="fieldset space-y-1 mt-6 w-full">
+          <div className="space-y-3">
+            <label className="text-xs  font-medium">Your Email</label>
+            <input
+              type="email"
+              className="input w-full border-2 mt-2 outline-none border-[#CBD5E1]  shadow-none"
+              placeholder="youremail@gmail.com"
+              {...register("email", { required: true })}
+            />
+            {errors?.email?.type === "required" && (
+              <p className="text-error font-bold">Email is required !</p>
+            )}
+          </div>
+          <div className="space-y-3">
+            <label className="text-xs  font-medium">Password</label>
+            <input
+              type="password"
+              className="input w-full border-2 mt-2 outline-none border-[#CBD5E1]  shadow-none"
+              placeholder="Password"
+              {...register("password", { required: true,minLength:6 , maxLength: 8 })}
+            />
+            {errors?.password?.type === "required" && (
+              <p className="text-error font-bold">Password is required !</p>
+            )}
+            {
+              errors?.password?.type === "minLength" && <p className="text-error font-bold">Password should be 6 characeter longer or more</p>
+            }
+            {
+              errors?.password?.type === "maxLength" && <p className="text-error font-bold">Password should be 8 characeter  or short</p>
+            }
+          </div>
+          <div className="space-y-3">
+            <p className="text-s  underline">Forget Password?</p>
+            <button className="btn w-full bg-[#CAEB66]">Login</button>
+            <p>
+              Don’t have any account?{" "}
+              <Link className="underline text-blue-600" to={"/signup"}>
+                Register
+              </Link>{" "}
+            </p>
+          </div>
+          <p className="text-center text-xs">Or</p>
+          <LoginWithGoogle />
         </fieldset>
       </form>
     </div>
--- src/Pageses/Auth/Signup/Signup.jsx
+++ src/Pageses/Auth/Signup/Signup.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import LoginWithGoogle from "../LoginwithGoogle/loginWithGoogle";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export default function Signup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
       <div className="space-y-2">
@@ -10,37 +19,58 @@ export default function Signup() {
         <p>Register with Profast</p>
       </div>
       {/* form */}
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset className="fieldset space-y-1 mt-6 w-full">
           <div className="space-y-3">
             <label className="text-xs  font-medium">Your Name</label>
             <input
+              type="text"
               className="input w-full border-2 mt-2 outline-none border-[#CBD5E1]  shadow-none"
               placeholder="Your Name"
+              {...register("yourName", { required: "Name is required", maxLength: 20 })}
             />
+            {errors?.yourName  && <p className="text-error">{errors?.yourName?.message}</p>  }
           </div>
           <div className="space-y-3">
             <label className="text-xs  font-medium">Your Email</label>
             <input
+            type="email"
               className="input w-full border-2 mt-2 outline-none border-[#CBD5E1]  shadow-none"
               placeholder="youremail@gmail.com"
+              {...register("email", { required: "Email is required" })}
             />
+            {errors?.email  && <p className="text-error">{errors?.email?.message}</p>  }
           </div>
           <div className="space-y-3">
             <label className="text-xs  font-medium">Password</label>
             <input
+            type="TEXT"
               className="input w-full border-2 mt-2 outline-none border-[#CBD5E1]  shadow-none"
               placeholder="Password"
+              {
+                ...register("password" , {required:"Password is required" , 
+                  minLength :{
+                    value:8 , message:"Password must be 8 character or longer."
+                  },
+              
+                  pattern:{
+                    value:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])/,
+                    message:"বড় হাতের, ছোট হাতের, নম্বর ও বিশেষ চিহ্ন থাকতে হবে"
+                  }
+                })
+              }
+               aria-invalid={errors.mail ? "true" : "false"}
             />
+            {errors?.password  && <p className="text-error">{errors?.password?.message}</p>  }
           </div>
           <div className="space-y-3">
             <p className="text-s  underline">Forget Password?</p>
             <button className="btn w-full bg-[#CAEB66]">Login</button>
             <p>
-              Don’t have any account?{" "}
-              <Link className="underline text-blue-600" to={"/signup"}>
-                Register
-              </Link>{" "}
+              Allready have an account. 
+              <Link className="underline text-blue-600" to={"/login"}>
+                Login
+              </Link>
             </p>
           </div>
           <p className="text-center text-xs">Or</p>
